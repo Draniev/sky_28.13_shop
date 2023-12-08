102e14ea91a2f1ebe22892061160e6b33fc2eae8
Sergei Popov <sergei.v.popov@icloud.com>
2023-12-09 01:44:19 +0300

    Solves task1 and adds tests for class Items

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,23 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def item_factory():
+    return Item('testname', 10_000, 5)
+
+
+def test_all_len(item_factory):
+    assert len(Item.all) == 1
+
+
+def test_total_price(item_factory):
+    assert item_factory.calculate_total_price() == 50_000
+
+
+def test_payrate(item_factory):
+    Item.pay_rate = 0.5
+    item_factory.apply_discount()
+    assert item_factory.price == 5_000
